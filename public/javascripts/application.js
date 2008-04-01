99e9a1622ceb4417acd5436a3703eeb3c8567dca
dinshawd <dinshawd@b28dfc24-0849-0410-8269-a052659aad68>
2008-04-01 03:23:18 +0000

    stuff
    git-svn-id: svn+ssh://74.53.208.111/home/dinshawd/svn-repos/sites/culinaryconcepts.com/trunk@13 b28dfc24-0849-0410-8269-a052659aad68

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,54 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+function addLoadEvent(func) {
+	var oldonload = window.onload;
+	if (typeof window.onload != 'function') {
+		window.onload = func;
+	} else {
+		window.onload = function() {
+			if (oldonload){
+				oldonload();
+			}
+			func();
+		}
+	}
+}
+
+function preparePopups() {
+
+	if (!document.getElementsByTagName) return false;
+	if (!document.getElementById) return false;
+
+	var links = document.getElementsByTagName("a");
+
+	for ( var i=0; i < links.length; i++) {
+		//Checkif popup is in the styles
+		if(links[i].className.indexOf("popup") != -1 ) {
+			title = links[i].getAttribute("title")			
+			
+			links[i].onclick = function() {	
+				return popup(this,title,this.className);
+			}
+			links[i].onkeypress = links[i].onclick;
+		}
+
+	}
+}
+
+function popup(target,title,classname) {
+	if(classname.indexOf("full") != -1 ) {
+		w = 1000
+		h = 800
+	}else{
+		w = 640
+		h = 480
+	}
+	
+	attributes = 'width='+w+',height='+h+',scrollbars=yes,resizable=yes'
+	window.open(target,'NextGenPopup',attributes)
+	return false
+}
+
+addLoadEvent(preparePopups);
+// don't know what this is for -= vajPlayer i think
+AC_FL_RunContent = 0;
