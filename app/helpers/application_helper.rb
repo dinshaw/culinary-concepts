118dd81e152f802a286e0a71f17e6d321ccaf15f
dinshawd <dinshawd@b28dfc24-0849-0410-8269-a052659aad68>
2008-04-02 03:30:03 +0000

    layout:
    git-svn-id: svn+ssh://74.53.208.111/home/dinshawd/svn-repos/sites/culinaryconcepts.com/trunk@18 b28dfc24-0849-0410-8269-a052659aad68

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+    
+  def nav_image_path(page)
+    "nav/rollovers/#{page}-#{nav_img_suffix(page)}.gif"
+  end
+  
+  def nav_img_suffix(page)
+    page = @controller.action_name == page ? "on" : "off" 
+  end
 end
