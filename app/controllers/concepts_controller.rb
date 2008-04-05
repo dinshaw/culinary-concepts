51345e6b26aa713cdd647c6c382b4f579aceb70e
dinshawd <dinshawd@b28dfc24-0849-0410-8269-a052659aad68>
2008-04-05 07:48:47 +0000

    bg
    git-svn-id: svn+ssh://74.53.208.111/home/dinshawd/svn-repos/sites/culinaryconcepts.com/trunk@84 b28dfc24-0849-0410-8269-a052659aad68

--- app/controllers/concepts_controller.rb
+++ app/controllers/concepts_controller.rb
@@ -4,17 +4,14 @@ class ConceptsController < ApplicationController
   # GET /concepts/1.xml
   def show
     @concept = Concept.find(params[:id])
-    @image_type = "concept"
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @concept }
     end
   end
   
-  def quote
-    @image_type = "start"    
+  def quote  
     @concept = Concept.find(params[:id])
-
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @concept }
