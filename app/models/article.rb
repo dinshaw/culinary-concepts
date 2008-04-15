0b8eb535548ed05a0aa0107ae22d1713143629e3
dinshawd <dinshawd@b28dfc24-0849-0410-8269-a052659aad68>
2008-04-15 03:15:55 +0000

    add pdf to article
    git-svn-id: svn+ssh://74.53.208.111/home/dinshawd/svn-repos/sites/culinaryconcepts.com/trunk@116 b28dfc24-0849-0410-8269-a052659aad68

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,6 +8,6 @@ class Article < ActiveRecord::Base
   acts_as_state_machine :initial => :active
   state :active
   
-  attachment_names :img_publication,:img_article
+  attachment_names :img_publication,:img_article, :content_file
     
 end
