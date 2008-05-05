935545144a3bb8a51f26ea6ed8dc228e543ae586
dinshawd <dinshawd@b28dfc24-0849-0410-8269-a052659aad68>
2008-05-05 18:06:16 +0000

    press vp
    git-svn-id: svn+ssh://74.53.208.111/home/dinshawd/svn-repos/sites/culinaryconcepts.com/trunk@135 b28dfc24-0849-0410-8269-a052659aad68

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,7 @@ class Article < ActiveRecord::Base
     
   acts_as_list
   
-  validates_presence_of :title, :body, :publication_name, :date_published
+  validates_presence_of :title, :date_published
   
   acts_as_state_machine :initial => :active
   state :active
