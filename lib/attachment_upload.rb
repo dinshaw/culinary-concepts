5387ee28ced69d9acbb246f2853a90177ed3abed
dinshawd <dinshawd@b28dfc24-0849-0410-8269-a052659aad68>
2008-05-15 05:33:37 +0000

    attachment upload
    git-svn-id: svn+ssh://74.53.208.111/home/dinshawd/svn-repos/sites/culinaryconcepts.com/trunk@160 b28dfc24-0849-0410-8269-a052659aad68

--- lib/attachment_upload.rb
+++ lib/attachment_upload.rb
@@ -53,7 +53,7 @@ module AttachmentUpload
      # Remove the attachemnt if the checkbox is checked and there is a attachment
      eval("self.attachments.#{attachment_name}.destroy rescue false") if eval("remove_#{attachment_name} == '1' && self.attachments.#{attachment_name} rescue false") 
      
-    if eval("uploaded_data_#{attachment_name} && uploaded_data_#{attachment_name}.size > 0")   
+    if eval("uploaded_data_#{attachment_name} && uploaded_data_#{attachment_name}.size > 0 rescue false")   
       eval("self.attachments.#{attachment_name}.destroy if self.attachments.#{attachment_name}")
       @attachment.uploaded_data = eval("uploaded_data_#{attachment_name}")
       @attachment.attachment_type = attachment_name.to_s
