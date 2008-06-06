cb581794a6e4000a81c57a2b9613c45839d09072
dinshawd <dinshawd@b28dfc24-0849-0410-8269-a052659aad68>
2008-06-06 14:51:34 +0000

    press note
    git-svn-id: svn+ssh://74.53.208.111/home/dinshawd/svn-repos/sites/culinaryconcepts.com/trunk@170 b28dfc24-0849-0410-8269-a052659aad68

--- lib/attachment_upload.rb
+++ lib/attachment_upload.rb
@@ -9,7 +9,7 @@ module AttachmentUpload
   # for create
   def save_with_attachments
     begin 
-      @@att_names.each { |attachment| do_attachment(attachment) }
+      self.class.att_names.each { |attachment| do_attachment(attachment) }
       save!
     rescue 
       add_errors(@attachment)
@@ -18,15 +18,15 @@ module AttachmentUpload
 
   # for update
   def update_with_attachments(params) 
-    # begin
+    begin
       self.transaction do
         update_attributes(params)
         self.class.att_names.each { |attachment| do_attachment(attachment) }
         save!
       end 
-    # rescue 
-    #   add_errors(@attachment)
-    # end 
+    rescue 
+      add_errors(@attachment)
+    end 
   end
 
   # popuplates the Attachment.new
@@ -35,12 +35,16 @@ module AttachmentUpload
      # Remove the attachemnt if the checkbox is checked and there is a attachment
      eval("self.attachments.#{attachment_name}.destroy") if eval("remove_#{attachment_name} == '1' && self.attachments.#{attachment_name}") 
      
+    #  If the file was submitied for upload
     if eval("uploaded_data_#{attachment_name} && uploaded_data_#{attachment_name}.size > 0")   
+      # Delete the atachment if it exists
       eval("self.attachments.#{attachment_name}.destroy if self.attachments.#{attachment_name}")
       @attachment.uploaded_data = eval("uploaded_data_#{attachment_name}")
       @attachment.attachment_type = attachment_name.to_s
+      # dont kow if i need this anymore
       @attachment.thumbnails.clear 
-      @attachment.save! 
+      # need to catch this exception and add it to errors
+      @attachment.save!
       self.attachments << @attachment
     end
     @attachment
